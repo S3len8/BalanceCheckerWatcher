ba17c9c1872ce291d9a779355d92a929fb1d80b7
Denys <glushychden.10@gmail.com>
2026-05-09 16:21:38 +0300

    Written file for Bitget, all working.

--- bitget.py
+++ bitget.py
@@ -47,7 +47,7 @@ async def get_balance_all_assets():
 
 
 get_balance_all_assets = asyncio.run(get_balance_all_assets())
-print(get_balance_all_assets)
+# print(get_balance_all_assets)
 
 
 async def get_balance_spot():
@@ -82,6 +82,25 @@ async def get_balance_spot():
 get_balance_spot = asyncio.run(get_balance_spot())
 print(get_balance_spot)
 
+
+async def list_symbols_spot():
+    """ Function for listing all available symbols in spot Bitget """
+    list_symbols_spot = [symbol['coin'] for symbol in get_balance_spot.get("data")]
+    return list_symbols_spot
+
+
+list_symbols_spot = asyncio.run(list_symbols_spot())
+print(list_symbols_spot)
+
+
+async def free_assets():
+    free_assets = [price.get("available") for price in get_balance_spot.get("data")]
+    return free_assets
+
+free_assets = asyncio.run(free_assets())
+print(free_assets)
+
+
 async def calc_all_assets():
     """ Function for calculation assets Bitget """
     result = {}
@@ -95,44 +114,52 @@ calc_all_assets = asyncio.run(calc_all_assets())
 print(calc_all_assets)
 
 
-async def get_current_price():
-    """ Function for getting current price Bitget """
-    endpoint = "/api/v2/spot/market/tickers"
-    timestamp = str(int(time.time() * 1000))
-    prehash_str = f"{timestamp}GET{endpoint}"
+async def get_price(session, symbol) -> float:
+    """ Function for getting prices from Binance """
+    if symbol == "USDT":
+        return "1.0"
+    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
+    async with session.get(url) as response:
+        data = await response.json()
+        if data.get("code") == -1121:
+            return "0.0"
+        return data.get("price")
 
-    # ВАЖНО: Bitget V2 требует Base64
-    mac = hmac.new(
-        API_SECRET.encode('utf-8'),
-        prehash_str.encode('utf-8'),
-        hashlib.sha256
-    )
-    signature = base64.b64encode(mac.digest()).decode('utf-8')
 
-    headers = {
-        "ACCESS-KEY": API_KEY,
-        "ACCESS-SIGN": signature,
-        "ACCESS-TIMESTAMP": timestamp,
-        "ACCESS-PASSPHRASE": PASSWORD,
-        "Content-Type": "application/json"
-    }
-
-    url = f"{SPOT_AND_FUTURES_BASE_URL}{endpoint}"
-
+async def create_session_bitget(symbols_list: list):
+    """ Function for creating sessions for get current prices in Bitget exchange """
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as response:
-            return await response.json()
+        tasks = []
+        for symbol in symbols_list:
+            tasks.append(get_price(session, symbol))
+
+        result = await asyncio.gather(*tasks)
+        return result
 
 
-
-get_current_price = asyncio.run(get_current_price())
-print(get_current_price)
-
+create_session_bitget = asyncio.run(create_session_bitget(list_symbols_spot))
+print(create_session_bitget)
 
 async def calc_price_spot():
     """ Function for calculation price spot assets Bitget """
+    result = []
+    for price, symbol in zip(free_assets, create_session_bitget):
+        calc_balance = float(price) * float(symbol)
+        result.append(calc_balance)
+    return result
+
+
+calc_price_spot = asyncio.run(calc_price_spot())
+print(calc_price_spot)
+
+
+async def compare_price_with_symbols_spot():
+    """ Function for comparing price spot assets Bitget """
     result = {}
-    for data in get_balance_spot.get("data"):
-        ticker = data["coin"]
-        available = data["available"]
-        pass
+    for price, symbol in zip(calc_price_spot, list_symbols_spot):
+        result.update({symbol: price})
+    return result
+
+
+compare_price_with_symbols_spot = asyncio.run(compare_price_with_symbols_spot())
+print(compare_price_with_symbols_spot)
